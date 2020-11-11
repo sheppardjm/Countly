1dd7cacb1be571061625b5821d723bef97464f70
Jamison Sheppard <jsheppard@neucadia.com>
2020-11-11 12:11:38 -0500

    Countly app using flux pub/sub model

--- App.js
+++ App.js
@@ -1,21 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'; 
+import { 
+  AppRegistry, 
+  StyleSheet, 
+  Text, 
+  View, 
+  TouchableOpacity 
+} from 'react-native'; 
+ 
+import { increment, decrement, zero } from './src/actions';
+import TallyStore from './src/TallyStore';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+class App extends Component { 
+  constructor(props){
+    super(props);
+    this.state = {
+      tally: TallyStore.getTally()
+    };
+    this.updateState = this.updateState.bind(this);
+  }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  componentDidMount() {
+    TallyStore.addChangeListener(this.updateState);
+  }
+
+  componentWillUnmount() {
+    TallyStore.removeChangeListener(this.updateState);
+  }
+
+  updateState() {
+    this.setState({
+      tally: TallyStore.getTally()
+    });
+  }
+
+  render() { 
+    return ( 
+      <View style={styles.container}> 
+        <Text style={styles.appName}> 
+          Countly 
+        </Text> 
+        <Text style={styles.tally}> 
+          Tally: {this.state.tally.count} 
+        </Text> 
+        <TouchableOpacity onPress={increment} style={styles.button}> 
+          <Text style={styles.buttonText}> 
+            + 
+          </Text> 
+        </TouchableOpacity> 
+        <TouchableOpacity onPress={decrement} style={styles.button}> 
+          <Text style={styles.buttonText}> 
+            - 
+          </Text> 
+        </TouchableOpacity> 
+        <TouchableOpacity onPress={zero} style={styles.button}> 
+          <Text style={styles.buttonText}> 
+            0 
+          </Text> 
+        </TouchableOpacity> 
+      </View> 
+    ); 
+  } 
+} 
+ 
+const styles = StyleSheet.create({ 
+  container: { 
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    backgroundColor: '#F5FCFF' 
+  }, 
+  appName: { 
+    fontSize: 20, 
+    textAlign: 'center', 
+    margin: 10 
+  }, 
+  tally: { 
+    textAlign: 'center', 
+    color: '#333333', 
+    marginBottom: 20, 
+    fontSize: 25 
+  }, 
+  button: { 
+    backgroundColor: 'blue', 
+    width: 100, 
+    marginBottom: 20, 
+    padding: 20 
+  }, 
+  buttonText: { 
+    color: 'white', 
+    textAlign: 'center', 
+    fontSize: 20 
+  } 
+}); 
+
+export default App;
+AppRegistry.registerComponent('App', () => App); 
+
